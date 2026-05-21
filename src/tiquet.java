d3d891a43808ddbb3d7ea9eb8901ea0071e68192
AFC <albertflori2007@gmail.com>
2026-05-21 17:57:21 +0200

    Tiquet V2, ja te per imprimir

--- src/tiquet.java
+++ src/tiquet.java
@@ -34,6 +34,38 @@ public class tiquet {
         this.totalFinal = this.totalFinal + linia.getPreuFinal();
     }
 
+    public void imprimirTiquet() {
+        System.out.println("\n========================================");
+        System.out.println("          BOTIGA DE ROBA TPV          ");
+        System.out.println("========================================");
+        System.out.println("Data: " + this.dataCompra);
+        System.out.println("Client (DNI): " + this.dniClient);
+        System.out.println("----------------------------------------");
+        System.out.println("Codi  Quantitat  Preu/unitat   IVA%   Total");
+        System.out.println("----------------------------------------");
+
+        // BUCLE FOR PER IMPRIMIR CADA ARTICLE
+        for (int i = 0; i < this.linies.size(); i++) {
+            
+            lineaTiquet linea = this.linies.get(i);
+
+            System.out.printf("%-6d %-10d %-8.2f %-6.0f %.2f€\n",  // PRINTF FA QUE ES VEGI EN COLUMNES
+                linea.getIdArticle(), 
+                linea.getQuantitat(), 
+                (linea.getPreuBase() / linea.getQuantitat()), // PREU PER UNITAT
+                linea.getIvaCalculat(), 
+                linea.getPreuFinal()
+            );
+        }
+
+        System.out.println("----------------------------------------");
+        System.out.printf("SUBTOTAL (Sense IVA): %.2f€\n", this.totalBase);
+        System.out.printf("TOTAL IVA:            %.2f€\n", this.totalIva);
+        System.out.println("========================================");
+        System.out.printf("TOTAL A PAGAR:        %.2f€\n", this.totalFinal);
+        System.out.println("========================================\n");
+    }
+
     //setter i getter ID
     public void setId(int id) {
         this.id = id;
@@ -59,7 +91,7 @@ public class tiquet {
         return dniClient;
     }
 
-    //setter i getter dniClient
+    //setter i getter totalBase
     public void setTotalBase(double totalBase){
         this.totalBase = totalBase;
     }
